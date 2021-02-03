386a615ed8d6b34d864586ac329edc0811c6ccd3
MMarioB <kSNW8Q5DGT>
2021-02-03 13:27:23 +0100

    agenda fin

--- agenda.py
+++ agenda.py
@@ -1,6 +1,8 @@
 import pymysql
 
 
+# Metodos
+
 def menu():
     print("*" * 20)
     print("- Menú Agenda -")
@@ -15,52 +17,135 @@ def menu():
     return opc
 
 
+def lee_entero():
+    while True:
+        entrada = input("Introduce una opcion: ")
+        try:
+            entrada = int(entrada)
+            return entrada
+        except ValueError:
+            print("Error!! Has introducido algo que no es un numero")
+
+
+def validarEdad():
+    while True:
+        entrada = input("Introduce una edad\n ")
+        try:
+            entrada = int(entrada)
+            return entrada
+        except ValueError:
+            print("Error!! Has introducido algo que no es una edad")
+
+
 def continuar():
     print("*" * 20)
     print("Quieres continuar?")
     print("[1] - Seguir")
     print("[0] - Salir")
     print("*" * 20)
-    opc = int(input("Elige una opcion:\n"))
-    return opc
+    opc = lee_entero()
+    while True:
+        if opc > 1:
+            opc = lee_entero()
+        else:
+            return opc
 
 
 def alta():
     nombre = input("Introduce el nombre\n")
-    apellidos = input("Introduce el apellido\n")
+    while True:
+        if not nombre:
+            print("Cadena vacia")
+            nombre = input("Introduce el nombre\n")
+        else:
+            break
+    apellido = input("Introduce el apellido\n")
+    while True:
+        if not apellido:
+            print("Cadena vacia")
+            apellido = input("Introduce el apellido\n")
+        else:
+            break
     telefono = input("Introduce el telefono\n")
-    edad = input("Introduce la edad\n")
-    consulta = "INSERT INTO agenda_db.contacto VALUES('" + nombre + "','" + apellidos + "'," + "'" + telefono + "'," + edad + ") "
-    print(consulta)
+    while True:
+        if telefono.isdigit():
+            break
+        else:
+            telefono = input("Introduce el telefono de nuevo\n")
+    validaEdad = validarEdad()
+    edad = str(validaEdad)
+    consulta = "INSERT INTO agenda_db.contacto VALUES('" + nombre + "','" + apellido + "'," + "'" + telefono + "'," + edad + ") "
     cursor.execute(consulta)
+    if cursor.execute(consulta) == 0:
+        print("No hay registros a los que aplicar esta consulta")
+    else:
+        print("Consulta realizada")
     conexion.commit()
 
 
 def baja():
     apellido = input("Introduce el apellido del contacto que quieres borrar")
-    consulta = "DELETE FROM agenda_db.contacto WHERE apellidos=" + "'" + apellido + "'"
-    print(consulta)
+    while True:
+        if not apellido:
+            print("Cadena vacia")
+            apellido = input("Introduce el apellido de nuevo\n")
+        else:
+            break
+    consulta = "DELETE FROM agenda_db.contacto WHERE apellido=" + "'" + apellido + "'"
     cursor.execute(consulta)
+    if cursor.execute(consulta) == 0:
+        print("No hay registros a los que aplicar esta consulta")
+    else:
+        print("Consulta realizada")
     conexion.commit()
 
 
 def modificar():
     nombreN = input("Introduce el nombre nuevo del contacto")
+    while True:
+        if not nombreN:
+            print("Cadena vacia")
+            nombreN = input("Introduce el nombre de nuevo\n")
+        else:
+            break
     telefonoN = input("Introduce el telefono nuevo del contacto")
-    edadN = input("Introduce la edad nueva del contacto")
+    while True:
+        if telefonoN.isdigit():
+            break
+        else:
+            telefonoN = input("Introduce el telefono de nuevo\n")
+    validaEdad = validarEdad()
+    edadN = str(validaEdad)
     apellido = input("Introduce el apellido del contacto que quieres modificar")
+    while True:
+        if not apellido:
+            print("Cadena vacia")
+            apellido = input("Introduce el apellido de nuevo\n")
+        else:
+            break
     consulta = "UPDATE agenda_db.contacto SET nombre=" + "'" + nombreN + "', " + "telefono=" + "'" + telefonoN + "', " + "edad=" \
-               + edadN + " WHERE apellidos='" + apellido + "'"
-    print(consulta)
+               + edadN + " WHERE apellido='" + apellido + "'"
     cursor.execute(consulta)
+    if cursor.execute(consulta) == 0:
+        print("No hay registros a los que aplicar esta consulta")
+    else:
+        print("Consulta realizada")
     conexion.commit()
 
 
 def buscar():
     apellido = input("Introduce el apellido del contacto que quieres buscar")
-    consulta = "SELECT * FROM agenda_db.contacto WHERE apellidos='" + apellido + "'"
-    print(consulta)
-    dato = cursor.execute(consulta)
+    while True:
+        if not apellido:
+            print("Cadena vacia")
+            apellido = input("Introduce el apellido de nuevo\n")
+        else:
+            break
+    consulta = "SELECT * FROM agenda_db.contacto WHERE apellido='" + apellido + "'"
+    if cursor.execute(consulta) == 0:
+        print("No hay registros a los que aplicar esta consulta")
+    else:
+        print("Consulta realizada")
     registros = cursor.fetchall()
     for registro in registros:
         print("***********************************")
@@ -76,6 +161,10 @@ def buscar():
 def mostrar():
     consulta = "SELECT * FROM agenda_db.contacto"
     cursor.execute(consulta)
+    if cursor.execute(consulta) == 0:
+        print("No hay registros a los que aplicar esta consulta")
+    else:
+        print("Consulta realizada")
     registros = cursor.fetchall()
     for registro in registros:
         print("***********************************")
@@ -88,8 +177,10 @@ def mostrar():
     conexion.commit()
 
 
+# Programa
 print("Empezamos")
 
+# Conexion
 try:
     conexion = pymysql.connect(host='localhost',
                                user='root',
@@ -104,46 +195,48 @@ cursor = conexion.cursor()
 
 cursor.execute("CREATE TABLE IF NOT EXISTS agenda_db.contacto (nombre TEXT, apellido TEXT, telefono TEXT, edad INT)")
 
+# Menu
 salir = False
-salirAlta = False
-salirBaja = False
-salirModificar = False
-salirBuscar = False
-seguir = ''
 while not salir:
     opcion = menu()
     if opcion == 1:
-        alta()
-        op1 = continuar()
-        while op1 != 0:
+        while True:
             alta()
             op1 = continuar()
+            if op1 == 0:
+                break
+            elif op1 == 1:
+                alta()
+            else:
+                print("Has introducido", op1, " Seguimos")
 
     elif opcion == 2:
-        baja()
-        op1 = continuar()
-        while op1 != 0:
+        while True:
             baja()
             op1 = continuar()
+            if op1 == 0:
+                break
+            elif op1 == 1:
+                baja()
     elif opcion == 3:
-        modificar()
-        op1 = continuar()
-        while op1 != 0:
+        while True:
             modificar()
             op1 = continuar()
+            if op1 == 0:
+                break
+            elif op1 == 1:
+                modificar()
     elif opcion == 4:
-        buscar()
-        op1 = continuar()
-        while op1 != 0:
+        while True:
             buscar()
             op1 = continuar()
+            if op1 == 0:
+                break
+            elif op1 == 1:
+                buscar()
     elif opcion == 5:
         mostrar()
     elif opcion == 0:
-        op1 = continuar()
-        while op1 != 0:
-            opcion = menu()
-            op1 = continuar()
         salir = True
         conexion.close()
     else:
